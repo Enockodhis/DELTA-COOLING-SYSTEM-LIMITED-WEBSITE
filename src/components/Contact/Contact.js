8607f9336c9dc06973e11bda48d1abf26728837a
Enock Omondi <eoomondi@safaricom.co.ke>
2024-09-18 13:49:07 +0300

    deltawebsite codes

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,85 @@
+import React, { useRef } from 'react';
+import './Contact.css';
+import { MdLocationOn, MdArrowDownward } from 'react-icons/md';
+import { FaPhoneAlt, FaEnvelope, FaGlobe } from 'react-icons/fa';
+import emailjs from '@emailjs/browser';
+
+const Contact = () => {
+  const form = useRef();
+  
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_mia9ga2', 'template_xk3q6qn', form.current, 'ezvhfv54TLDUcU8ud')
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          e.target.reset();
+          alert('Email Sent!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        }
+      );
+  };
+
+  return (
+    <section id='contactPage'>
+      <div className='contactInfo'>
+        <h2 className='contactHeading'>CONNECT WITH US</h2>
+        <div className='contactDetails'>
+          <p className='phoneNumber'><strong>Phone:</strong></p>
+          <ul className='numberList'>
+            <li><FaPhoneAlt className='contactIcon' />+254 733 823 277</li>
+            <li><FaPhoneAlt className='contactIcon' />+254 713 473 009</li>
+          </ul>
+          <p className='emailAddress'><strong>Email Address:</strong></p>
+          <ul className='emailList'>
+            <li><FaEnvelope className='contactIcon' />deltacoolingsystemsltd@gmail.com</li>
+            <li><FaEnvelope className='contactIcon' />delta.cooling@yahoo.com</li>
+            <li><FaEnvelope className='contactIcon' />info@deltacooling.co.ke</li>
+          </ul>
+          <p className='website'><strong>Website:</strong></p>
+          <p><FaGlobe className='contactIcon' /><a href="http://www.deltacooling.co.ke" className='web-link'>www.deltacooling.co.ke</a></p>
+        </div>
+        <div className="contact-Location">
+          <h3>Office Location</h3>
+          <p>CRYSTAL ALUMINIUM COMPLEX (GODOWN NO.2)</p>
+          <p>BEHIND BASCO PAINT FACTORY, OLD AIRPORT NORTH ROAD</p>
+          <p>OFF MOMBASA ROAD.</p>
+          <p>P.O BOX 12915 – 00100</p>
+          <p>NAIROBI.</p>
+        </div>
+        <div className='mapLocation'>
+          <a href='https://maps.app.goo.gl/ZUtGqZEwQCbcp1WJA' target='_blank' rel='noopener noreferrer' className='direction-link'>
+            <div className='locationIconMain'>
+              <MdLocationOn className='locationIcon1' />
+              <MdArrowDownward className='locationIcon2' />
+            </div>
+            Click For Directions
+          </a>
+        </div>
+      </div>
+      <div className='contactForm'>
+        <form className='contactFrm' ref={form} onSubmit={sendEmail}>
+          <label htmlFor='fullName' className='form-label'>Full Name</label>
+          <input type='text' className='form-control' placeholder='Your Name' name='your_name' required />
+          <label htmlFor='phone' className='form-label'>Phone</label>
+          <input type='text' className='form-control' placeholder='+254 733 823 277' name='phone_number' required />
+          <label htmlFor='email' className='form-label'>Email</label>
+          <input type='email' className='form-control' placeholder='example@email.com' name='your_email' required />
+          <label htmlFor='message' className='form-label'>Message</label>
+          <textarea className='form-control' name='message' rows='6' placeholder='Type your message here' required></textarea>
+          <div className='submit'>
+            <button className='submitBtn' type='submit'>
+              <span>Send Message</span>
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+}
+
+export default Contact;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './Footer.css';
+import logo from '../../assets/deltalogo.png';
+import { FaHome, FaInfo, FaServicestack, FaPhoneAlt } from 'react-icons/fa';
+
+const Footer = () => {
+  return (
+    <footer className='Footer'>
+     <div className='Footer-Left'>
+        <div className='Footer-details'>
+             <img src={logo} alt='logo' className='footerLogo'/>
+             <div className='Footer-orgName'>
+                  <h1>DELTA COOLING SYSTEMS LIMITED</h1>
+                  <div className='underline-footer'></div>
+                  <h2>Your Ultimate and Trusted Cooling Solutions Provider</h2>
+             </div>
+        </div>
+        <div className='Footer-copyright'>
+            Copyright  &#169;  2024 DELTA COOLING SYSTEMS LIMITED  |  All Rights Reserved
+        </div>
+     </div>
+     <div className='Footer-Right'>
+         <div className='footer-menu'>
+             <div className='homeFooter'>
+                 <a href='/'><FaHome className='navbar-icon' />Home</a>
+                 <br></br>
+             </div>
+             <div className='aboutFooter'>
+                <a href='/about'><FaInfo className='navbar-icon' />About</a>
+                <br></br>
+             </div>
+             <div className='servicesFooter'>
+                <a href='/services'><FaServicestack className='navbar-icon' />Services</a>
+                <br></br>
+             </div>
+             <div className='contactFooter'>
+                <a href='/contact-us'><FaPhoneAlt className='navbar-icon' />Contact Us</a>
+             </div>  
+         </div>
+     </div>
+    </footer>
+  )
+}
+
+export default Footer

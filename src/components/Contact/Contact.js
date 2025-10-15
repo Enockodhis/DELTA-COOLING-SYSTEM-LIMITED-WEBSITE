45b83fec32832ecf22cb18d8d917f6bc6ed9529b
JohnM- Oduor <oduorjohn104@gmail.com>
2025-10-15 07:54:38 +0300

    Location update

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -77,7 +77,7 @@ const Contact = () => {
         </div>
         <div className="contact-Location">
           <h3>Office Location</h3>
-          <p>UNIPRO COMPLEX UNIT 4</p>
+          <p>GODOWN NO.4 NEXT TO UNIPRO</p>
           <p>ROAD C BEHIND VISION PLAZA</p>
           <p>OFF MOMBASA ROAD.</p>
           <p>P.O BOX 12915 – 00100</p>
